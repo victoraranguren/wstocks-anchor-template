61fcdb49b960e71f5ce62a09598793f63422942e
Victor <victoraranguren.dev@gmail.com>
2026-01-23 13:49:15 -0400

    initialize_asset ix added and testedanchor deploy

--- programs/anchor-rwa-template/src/lib.rs
+++ programs/anchor-rwa-template/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, Token};
 
 declare_id!("jEXgKE9NWJihHqLVAoXZ4e2TSZ7KkV7kub8j4ojcmZC");
 
@@ -10,7 +11,74 @@ pub mod anchor_rwa_template {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
+
+    pub fn initialize_asset(
+        ctx: Context<InitializeAsset>,
+        asset_symbol: String,
+        asset_isin: String,
+        legal_doc_uri: String,
+        asset_type: AssetType,
+    ) -> Result<()> {
+        let asset_registry = &mut ctx.accounts.asset_registry;
+        let timestamp = Clock::get()?.unix_timestamp;
+        asset_registry.authority = ctx.accounts.owner.key();
+        asset_registry.mint = ctx.accounts.mint.key();
+
+        asset_registry.asset_symbol = asset_symbol;
+        asset_registry.asset_isin = asset_isin;
+        asset_registry.legal_doc_uri = legal_doc_uri;
+
+        asset_registry.creation_date = timestamp;
+
+        asset_registry.asset_type = asset_type;
+        asset_registry.bump = ctx.bumps.asset_registry;
+
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct InitializeAsset<'info> {
+    #[account(init, payer = owner, space = AssetRegistry::INIT_SPACE, seeds = [b"asset_registry", owner.key().as_ref()], bump)]
+    pub asset_registry: Account<'info, AssetRegistry>,
+    #[account(init, payer = owner, mint::decimals = 8, mint::authority = owner, mint::freeze_authority = owner)]
+    pub mint: Account<'info, Mint>,
+
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct AssetRegistry {
+    pub authority: Pubkey,
+    pub mint: Pubkey,
+
+    #[max_len(10)]
+    pub asset_symbol: String,
+    #[max_len(16)]
+    pub asset_isin: String,
+    #[max_len(200)]
+    pub legal_doc_uri: String,
+    pub creation_date: i64,
+
+    pub asset_type: AssetType,
+
+    pub bump: u8,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq, InitSpace)]
+pub enum AssetType {
+    Equity,
+    Debt,
+    RealEstate,
+    Commodity,
+}
--- tests/anchor-rwa-template.ts
+++ tests/anchor-rwa-template.ts
@@ -1,16 +1,59 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+import { min } from "bn.js";
 import { AnchorRwaTemplate } from "../target/types/anchor_rwa_template";
 
 describe("anchor-rwa-template", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.AnchorRwaTemplate as Program<AnchorRwaTemplate>;
-
+  const program = anchor.workspace
+    .AnchorRwaTemplate as Program<AnchorRwaTemplate>;
+  const wallet = anchor.getProvider().wallet;
   it("Is initialized!", async () => {
     // Add your test here.
     const tx = await program.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
+  });
+
+  it("Is AssetRegistry initialized!", async () => {
+    const assetRegistry = {
+      assetSymbol: "WAAPL",
+      assetIsin: "VE-WAAPL-001",
+      legalDocUri: "https://www.youtube.com/watch?v=MOl4s-VIuLQ",
+      assetType: { equity: {} },
+    };
+    const mint = anchor.web3.Keypair.generate();
+    const [assetRegistryPda, bump] =
+      anchor.web3.PublicKey.findProgramAddressSync(
+        [Buffer.from("asset_registry"), wallet.publicKey.toBuffer()],
+        program.programId
+      );
+
+    console.log("assetRegistry: ", assetRegistry);
+    console.log("assetRegistryPda: ", assetRegistryPda);
+    console.log("mint: ", mint.publicKey);
+    console.log("wallet: ", wallet.publicKey);
+    // Add your test here.
+    const tx = await program.methods
+      .initializeAsset(
+        assetRegistry.assetSymbol,
+        assetRegistry.assetIsin,
+        assetRegistry.legalDocUri,
+        assetRegistry.assetType
+      )
+      .accounts({
+        owner: wallet.publicKey,
+        assetRegistry: assetRegistryPda,
+        mint: mint.publicKey,
+      })
+      .signers([mint])
+      .rpc();
+    console.log("Your transaction signature", tx);
+
+    let assetRegistryAccount = await program.account.assetRegistry.fetch(
+      assetRegistryPda
+    );
+    console.log("assetRegistry: ", assetRegistryAccount);
   });
 });
